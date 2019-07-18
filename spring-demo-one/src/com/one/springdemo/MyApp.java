fd1bd599defae6131677e7120c5774584827fbf5
Mateusz <mateuszkochanek01@gmail.com>
2019-07-18 14:02:43 +0200

    #007.Bean used to call Coach methods (Inversion of Control)

--- spring-demo-one/src/com/one/springdemo/MyApp.java
+++ spring-demo-one/src/com/one/springdemo/MyApp.java
@@ -3,8 +3,11 @@ package com.one.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
+		
+		//Create a coach
 		Coach theCoach = new TrackCoach();
 		
+		//Use the object
 		System.out.println(theCoach.getDailyWorkout());
 
 	}
