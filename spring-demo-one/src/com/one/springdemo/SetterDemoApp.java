b6169d87efa18777c092195ad86cd102a3de3320
Mateusz <mateuszkochanek01@gmail.com>
2019-07-20 15:05:25 +0200

    #015.Setter injection used

---
+++ spring-demo-one/src/com/one/springdemo/SetterDemoApp.java
@@ -0,0 +1,20 @@
+package com.one.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		ClimbingCoach theCoach = context.getBean("myClimbingCoach", ClimbingCoach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		context.close();
+
+	}
+
+}
