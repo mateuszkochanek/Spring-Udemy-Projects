f6347f7937fa69545633b10db9d19b1750b03fde
Mateusz <mateuszkochanek01@gmail.com>
2019-07-20 20:05:28 +0200

    #018.Random Fortunes picking added

--- spring-demo-one/src/com/one/springdemo/HappyFortuneSrrvice.java
+++ spring-demo-one/src/com/one/springdemo/HappyFortuneSrrvice.java
@@ -1,10 +1,19 @@
 package com.one.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneSrrvice implements FortuneService {
 
+	Random random = new Random();
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day";
+		int x;
+		String[] Fortunes= new String[5];
+		for(int i = 0; i<5 ; i++) {
+			Fortunes[i] = "Fortune number= " + i;
+		}
+		x = random.nextInt(5);
+		return Fortunes[x];
 	}
 
 }
