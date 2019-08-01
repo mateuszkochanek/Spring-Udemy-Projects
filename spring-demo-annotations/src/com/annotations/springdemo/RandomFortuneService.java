88e63393ff5a1d5de1073ccc54bb70a3b207b277
Mateusz <mateuszkochanek01@gmail.com>
2019-08-01 16:20:20 +0200

    #010.Multiple FortuneServ created, using @Qualifiers for DI.

---
+++ spring-demo-annotations/src/com/annotations/springdemo/RandomFortuneService.java
@@ -0,0 +1,21 @@
+package com.annotations.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = {
+			"We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light./n- Plato",
+			"When you light a candle, you also cast a shadow./n- Ursula K. Le Guin ",
+			"Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering./n- Yoda"
+	};
+	private Random myRandom = new Random();
+	@Override
+	public String getFortune() {
+		return "Random Fortune";
+	}
+
+}
