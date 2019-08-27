e677636f0c28ca321827febb27bca23983f66068
Erthax <mateuszkochanek01@gmail.com>
2019-08-27 22:06:27 +0200

    #016.Spring Configuration with Java enabled

---
+++ spring-demo-annotations/src/com/annotations/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.annotations.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.annotations.springdemo")
+public class SportConfig {
+
+}
