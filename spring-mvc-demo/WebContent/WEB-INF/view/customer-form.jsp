b4db4b75a6fe7c47b9eb341fdea5b5a840675927
Erthax <mateuszkochanek01@gmail.com>
2019-09-09 12:17:55 +0200

    #012.String input in Integer field, making Int field required

--- spring-mvc-demo/WebContent/WEB-INF/view/customer-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/customer-form.jsp
@@ -23,10 +23,10 @@ Fill the form, (*) means that information is required.
 	First name: <form:input path="firstName"/><br><br>
 	Last name (*): <form:input path="lastName"/>
 	<form:errors path="lastName" cssClass="error"/><br><br>
-	Free passes: <form:input path="freePasses"/><br><br>
-	<form:errors path="freePasses" cssClass="error"/>
-	Postal code: <form:input path="postalCode"/><br><br>
-	<form:errors path="postalCode" cssClass="error"/>
+	Free passes: <form:input path="freePasses"/>
+	<form:errors path="freePasses" cssClass="error"/><br><br>
+	Postal code: <form:input path="postalCode"/>
+	<form:errors path="postalCode" cssClass="error"/><br><br>
 	<input type="submit" value="Submit"/>
 </form:form>
 
--- spring-mvc-demo/src/com/spring/demo/mvc/Customer.java
+++ spring-mvc-demo/src/com/spring/demo/mvc/Customer.java
@@ -12,9 +12,10 @@ public class Customer {
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than 0")
 	@Max(value=10, message="must be less than 11")
-	private int freePasses;
+	private Integer freePasses;
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
 	
@@ -37,10 +38,10 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	
