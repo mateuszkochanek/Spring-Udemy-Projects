367cdd58a0599eaf8f01aebb8954a4941bf5c53d
Erthax <mateuszkochanek01@gmail.com>
2019-09-23 21:02:40 +0200

    #016.New project for OneToMany created, Course class created

---
+++ hb-03-one-to-many/src/com/luv2code/hibernate/demo/DeleteDemo.java
@@ -0,0 +1,45 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+
+public class DeleteDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			
+			//get instructor by Id
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("Instructor: " + tempInstructor);
+			//delete the instructors
+			if(tempInstructor != null) {
+				System.out.println("Deleting: " + tempInstructor);
+				//it will delete instructor deails too
+				session.delete(tempInstructor);
+			}
+
+			session.getTransaction().commit();
+		}
+		finally{
+			factory.close();
+		}
+
+	}
+
+}
