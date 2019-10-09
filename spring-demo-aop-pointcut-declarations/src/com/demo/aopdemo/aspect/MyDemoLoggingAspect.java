b3a7e3da8c238ee11d4163442ac931792c4b7f70
Erthax <mateuszkochanek01@gmail.com>
2019-10-09 15:39:01 +0200

    #011.Pointcut expression declared and reused

--- spring-demo-aop-pointcut-declarations/src/com/demo/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/com/demo/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,15 +2,23 @@ package com.demo.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-	//@Before("execution(public void add*())")
-	@Before("execution( * com.demo.aopdemo.dao.*.*(..))")
+	@Pointcut("execution( * com.demo.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
-		System.out.println("\n @Before executing before add() methods");
+		System.out.println("\n -----> @Before executing before add() methods");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalitics() {
+		System.out.println("\n -----> @Before executing api analitics");
 	}
 }
